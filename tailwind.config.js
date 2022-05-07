ea408e584149fb0df252dd8c0bfac39b8e130b17
RafaelGoulartB <rafagoulartb@gmail.com>
2022-05-07 15:32:25 -0300

    🎨 style: adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* 
+  Explore configuration options docs https://tailwindcss.com/docs/configuration#configuration-options
+  Or check the default configuration https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+*/
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
