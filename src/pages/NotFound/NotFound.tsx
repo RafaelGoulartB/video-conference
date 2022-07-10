8e6cc463b0aa2eef16079677aa4d7931b8dd95ac
RafaelGoulartB <rafagoulartb@gmail.com>
2022-07-10 14:35:06 -0300

    📦 feat: creating routes of not found and redirect '/' to 'create-room'

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound: React.FC = () => {
+  return <p>NotFound</p>
+}
+
+export default NotFound
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,16 +2,19 @@ import Call from 'pages/Call/Call'
 import CreateRoom from 'pages/CreateRoom/CreateRoom'
 import EndCall from 'pages/EndCall/EndCall'
 import JoinRoom from 'pages/JoinRoom/JoinRoom'
+import NotFound from 'pages/NotFound/NotFound'
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 const Router: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="create-room" replace />} />
       <Route path="/create-room" element={<CreateRoom />} />
       <Route path="/join-room" element={<JoinRoom />} />
       <Route path="/call/:id" element={<Call />} />
       <Route path="/end-call" element={<EndCall />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
