ad2c310e9a1cb447cc9cacbd00ebfeb583523d25
RafaelGoulartB <rafagoulartb@gmail.com>
2022-07-11 13:18:26 -0300

    🎨 style: create tailwind theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Explore configuration options docs https://tailwindcss.com/docs/configuration#configuration-options
   Or check the default configuration https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
 */
@@ -6,7 +6,28 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      primary: '#49627d',
+      secondary: '#627b98',
+      gray: '#4a5767',
+      'dark-gray': '#131b26',
+      white: '#ffffff',
+      red: '#f8000f',
+      green: '#009d23'
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      }
+    }
   },
   plugins: []
 }
