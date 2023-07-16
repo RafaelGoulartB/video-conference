23c325471fa89346ba3a37d89a18f3ef013f8149
RafaelGoulartB <rafagoulartb@gmail.com>
2023-07-15 23:04:30 -0300

    🎨 style: add grid layout to handle with video call layout

--- src/pages/Call/Call.tsx
+++ src/pages/Call/Call.tsx
@@ -1,3 +1,5 @@
+import Room from 'components/Call/Room/Room'
+import Sidebar from 'components/Call/Sidebar/Sidebar'
 import ContainerStyle from 'components/Container/Container'
 import React from 'react'
 import { useParams } from 'react-router-dom'
@@ -5,7 +7,14 @@ import { useParams } from 'react-router-dom'
 const Call: React.FC = () => {
   const { callId } = useParams()
 
-  return <ContainerStyle>p</ContainerStyle>
+  return (
+    <ContainerStyle>
+      <div className="grid grid-cols-12 w-screen h-screen">
+        <Room />
+        <Sidebar />
+      </div>
+    </ContainerStyle>
+  )
 }
 
 export default Call
