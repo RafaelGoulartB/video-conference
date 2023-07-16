000687db0be8fade2e6352841ab7a2ef68a8ed10
RafaelGoulartB <rafagoulartb@gmail.com>
2023-07-15 23:03:56 -0300

    📦 feat: add customer classNames in container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,16 +3,22 @@ import React from 'react'
 interface ContainerProps {
   children?: React.ReactNode
   centerContent?: boolean
+  mainClassName?: string
+  className?: string
 }
 
 const ContainerStyle: React.FC<ContainerProps> = ({
   children,
-  centerContent = false
+  centerContent = false,
+  mainClassName,
+  className
 }) => {
   return (
-    <main className="flex w-screen h-screen bg-gradient-to-r from-bg-color-s to-bg-color-e">
+    <main
+      className={`flex w-screen h-screen bg-gradient-to-r from-bg-color-s to-bg-color-e ${mainClassName}`}
+    >
       <div
-        className={`container flex flex-col
+        className={`${className} container flex flex-col
           lg:max-w-none ${centerContent ? 'justify-center items-center' : ''}`}
       >
         {children}
